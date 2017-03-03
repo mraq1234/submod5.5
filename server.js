8902f42f75ac896b4ab255e8b4bc8511bf356af6
Mariusz Rak <rak.m.rak@gmail.com>
2017-03-03 21:16:04 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.use('/store', (req, res, next) => {
+    console.log('Jestem pośrednikiem przy żądaniu do /store ');
+});
+
+app.get('/store', (req, res) => {
+    res.send('To jest sklep.');
+});
+
+app.listen(3000);
+
+app.use((req, res, next) => {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
